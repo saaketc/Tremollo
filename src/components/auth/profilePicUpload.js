fa6cf54acd42de74694d77b2206d461036905b5b
Saaket <saaket123@outlook.com>
2020-06-09 23:54:10 +0530

    implements profile pic update

--- src/components/auth/profilePicUpload.js
+++ src/components/auth/profilePicUpload.js
@@ -2,14 +2,31 @@ import React from 'react'
 import TextForm from "../common/textForm";
 import logo from "../../logo/logo_lite_crop.png";
 import dataService from "../../services/dataServices";
+import colors from "../../config/colors";
 
 import { Container, Grid, Typography, Button } from "@material-ui/core";
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom'; 
+import { makeStyles } from "@material-ui/core/styles";
   
-const ProfilePicUpload = ({user}) => {
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontWeight: '600'
+  },
+  btn: {
+    color: colors.white,
+    border: `1px solid ${colors.primary}`,
+    backgroundColor: colors.primary,
+    "&:hover": {
+      backgroundColor: colors.primary,
+    }
+  }
+}));
+const ProfilePicUpload = (props) => {
   const history = useHistory();  
   const [file, setFile] = React.useState(null);
+  const classes = useStyles();
+  const { user, location } = props;
 
     const handleChange = ({ currentTarget }) => {
         const pic = currentTarget.files[0];
@@ -23,7 +40,10 @@ const ProfilePicUpload = ({user}) => {
         formData.append('userId', user.userId)
         const { data } = await dataService.putData('user/avatar', formData, 'multipart/form-data');
         // console.log(data.body);
-          history.push('/');
+          if (location.state) {
+            return history.push('/profile', user);
+          }
+          return history.push('/');
         }
         catch (e) {
             console.log(e.message);
@@ -33,21 +53,22 @@ const ProfilePicUpload = ({user}) => {
     return (
         <Container>
         
-      <Typography variant="h5">
-        Upload your profile pic
+      <Typography variant="h4" className={classes.title}>
+          {user.firstName} upload your profile pic
       </Typography>
       <br />
 
       <form onSubmit={handleSubmit} autoComplete="off">
-        <TextForm
-         
+          <TextForm
           
           type="file"
           name="file"
           required={true}
           onChange={handleChange}
-                />
-                <Button type='submit'>Upload</Button>
+          />
+          <br/>
+          <br/>
+                <Button className={classes.btn} type='submit'>Upload</Button>
         </form>        
         </Container>
     )
