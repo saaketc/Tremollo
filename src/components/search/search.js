d59a3b7d9203e4ea1d27cfb6dcb20a2154cd5ccb
Saaket <saaket123@outlook.com>
2020-06-10 17:06:51 +0530

    improves search bar adds circular edges

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,101 +1,68 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
-import { fade, makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import { useHistory } from "react-router-dom";
 
 import colors from "../../config/colors";
 
-
 const useStyles = makeStyles((theme) => ({
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
+  root: {
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    flexGrow: 1,
+    maxWidth: 900,
+    border: '1 px solid white',
+    borderRadius: '50px'
   },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 200,
-      },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 2,
+    [theme.breakpoints.up("xs")]: {
+      display: "block",
+      width: 200,
     },
+    
   },
-  list: {
-    listStyleType: "none",
-    padding: 0,
-    margin: 0,
+  iconButton: {
+    padding: 10,
   },
-  listItem: {
-    padding: "12px",
-    textDecoration: "none",
-    color: colors.black,
-    display: "block",
+  divider: {
+    height: 28,
+    margin: 4,
   },
 }));
-const Search = () => {
+
+export default function SearchBar() {
   const classes = useStyles();
   const history = useHistory();
-
   const [searchTerm, setSearchTerm] = React.useState("");
-  const handleChange = async ({ currentTarget }) => {
-    
+
+  const handleChange = ({ currentTarget }) => {
     setSearchTerm(currentTarget.value);
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!searchTerm)
-      return
     return history.push(`/search?q=${searchTerm}`);
   };
 
-
   return (
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon style={{ color: colors.black }} />
-      </div>
-      <form onSubmit={handleSubmit} autoComplete='off'>
-        <InputBase
-          placeholder="Search music..."
-          style={{ color: colors.black }}
-          name="searchTerm"
-          value={searchTerm}
-          onChange={handleChange}
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput,
-          }}
-          inputProps={{ "aria-label": "search" }}
-        />
-      </form>
-    </div>
+    <Paper component="form" className={classes.root} onSubmit={handleSubmit}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search for content or user..."
+        inputProps={{ "aria-label": "Search for content or user..." }}
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <IconButton
+        type="submit"
+        className={classes.iconButton}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
-};
-
-export default Search;
+}
