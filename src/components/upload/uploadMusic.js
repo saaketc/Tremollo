2b8b83e221aff25ec754e8411b8951293df4e111
Saaket <saaket123@outlook.com>
2020-06-18 04:20:48 +0530

    upload music ui changes

--- src/components/upload/uploadMusic.js
+++ src/components/upload/uploadMusic.js
@@ -7,12 +7,15 @@ import dataService from "../../services/dataServices";
 import { storageURL } from "../../config/storage";
 import colors from "../../config/colors";
 import TextForm from "../common/textForm";
-import { buttonStyleClose } from "../../config/buttonStyle";
+import { buttonStyleClose, buttonStyleOpen } from "../../config/buttonStyle";
 import darkTheme from "../../config/themes/dark";
+import MediaCard from "../common/mediaCard";
+import ReactLoading from "react-loading";
 
 const UploadMusic = ({ user }) => {
   const [fileData, setFileData] = React.useState({});
   const [thumbnail, setThumbnail] = React.useState("");
+  const [uploading, setUploading] = React.useState('');
 
   const formik = useFormik({
     initialValues: {
@@ -32,8 +35,8 @@ const UploadMusic = ({ user }) => {
           "content/upload/data",
           toSend
         );
-          console.log(data.body);
-          toast.success('Yaayy! Successfully uploaded.')
+        console.log(data.body);
+        toast.success("Yaayy! Successfully uploaded.");
       } catch (e) {
         console.log(e.message);
         toast.error("Oops! Something went wrong.");
@@ -44,6 +47,8 @@ const UploadMusic = ({ user }) => {
 
   const handleChange = async ({ currentTarget }) => {
     try {
+      setUploading('pending');
+
       let formData = new FormData();
       formData.append("file", currentTarget.files[0]);
       const { data } = await dataService.putData(
@@ -53,47 +58,63 @@ const UploadMusic = ({ user }) => {
       );
       console.log(data.body);
       setFileData(data.body);
+      setUploading('done');
     } catch (e) {
       toast.error("Something went wrong while uploading...");
       console.log(e.message);
     }
   };
-  const handlePickThumbnail = (thumbnail) => {
+  const handlePickThumbnail = (thumbnail, index) => {
+    
     return setThumbnail(thumbnail);
   };
 
   return (
-    <Container  style={{
-      color: 'white'
-    }}>
-      <form style={{
-        color: darkTheme.textColor
-      }}>
-        <input type="file" name="file" onChange={handleChange} />
+    <Container>
+      <form>
+        <Button style={buttonStyleOpen}><label for='upload'>Choose Music</label></Button>
+        <input id='upload' type="file" name="file" onChange={handleChange} style={{display: 'none'}}/>
       </form>
-      {Object.keys(fileData).length > 0 &&
-        fileData.fileThumbnails.split(",").map((t) => (
-          <Button onClick={() => handlePickThumbnail(t)}>
-            <img
-              src={storageURL + t}
-              alt="thumbnail"
-              width="100%"
-              height="100"
-              style={{
-                border: `5 px solid ${colors.primary}`,
-                borderRadius: "50%",
-              }}
-            />
-          </Button>
-        ))}
-          <br/>
-          <br/>
+      <br/>
+      <br />
+      {
+          Object.keys(fileData).length > 0 && 
+          <Typography variant='h4'>
+          Pick a thumbnail for your music video  
+          </Typography>
+        }
+      <Grid container spacing={6}>
+       
+        <br/>
+        <br/>
+        {
+          uploading === 'pending' ? <ReactLoading type='bars' color={colors.primary} height={150} width={150} /> :  (
+          <>
+            {Object.keys(fileData).length > 0 &&
+         
+              fileData.fileThumbnails.split(",").map((t, index) => (
+                <Grid item xs={12} md={3} lg={3}>
+                  <MediaCard
+                    data={{thumbnail: t, index}}
+                    image={storageURL + t}
+                    onClick={handlePickThumbnail}
+                    imageTitle="Thumbnail"
+                  />
+                </Grid>
+              ))}
+              </>
+          )
+       }
+      </Grid>
+      <br />
+      <br />
       <form onSubmit={formik.handleSubmit} autoComplete="off">
         <TextForm
           label="Title of the content"
           type="text"
           name="title"
           required={true}
+          fullWidth
           value={formik.values.title}
           onChange={formik.handleChange}
         />
@@ -105,6 +126,7 @@ const UploadMusic = ({ user }) => {
           multiline={true}
           rows="2"
           required={true}
+          fullWidth
           value={formik.values.caption}
           onChange={formik.handleChange}
         />
@@ -116,12 +138,13 @@ const UploadMusic = ({ user }) => {
           name="tags"
           placeholder="Add comma separated tags for your content"
           required={true}
+          fullWidth
           value={formik.values.tags}
           onChange={formik.handleChange}
         />
         <br />
 
-        <Button style={ buttonStyleClose } type="submit">
+        <Button style={buttonStyleClose} type="submit">
           Upload
         </Button>
       </form>
