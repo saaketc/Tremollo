c020d4e08e2c6794e45333f067dc8a00e42537bf
Saaket <saaket123@outlook.com>
2020-06-27 00:17:12 +0530

    adds border radius

--- src/config/buttonStyle.js
+++ src/config/buttonStyle.js
@@ -1,18 +1,22 @@
-import colors from './colors';
+import colors from "./colors";
 
 export const buttonStyleOpen = {
-    color: colors.primary,
-    border: `1px solid ${colors.primary}`,
-    "&:hover": {
-      backgroundColor: colors.white,
-    },
-}
-  
+  color: colors.primary,
+  border: `1px solid ${colors.primary}`,
+  borderRadius: "25px",
+  "&:hover": {
+    backgroundColor: colors.white,
+    
+  },
+};
+
 export const buttonStyleClose = {
-    color: colors.white,
-    border: `1px solid ${colors.primary}`,
-    background: colors.primary,
-    "&:hover": {
-      backgroundColor: colors.primary,
-    },
-  }
+  color: colors.white,
+  border: `1px solid ${colors.primary}`,
+  borderRadius: "25px",
+
+  background: colors.primary,
+  "&:hover": {
+    backgroundColor: colors.primary,
+  },
+};
