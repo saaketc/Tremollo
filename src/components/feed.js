f2b09a1e347f595dcc0ef060c1c18aef4cd4da13
Saaket <saaket123@outlook.com>
2020-06-29 18:03:06 +0530

    loader added

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,73 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { useHistory } from "react-router-dom";
 
-import dataService from '../services/dataServices';
-import { storageURL } from '../config/storage';
-import CardComponent from './common/cardComponent';
-import { encode } from '../utils/utilfunctions';
+import dataService from "../services/dataServices";
+import { storageURL } from "../config/storage";
+import CardComponent from "./common/cardComponent";
+import { encode } from "../utils/utilfunctions";
+import ReactLoading from "react-loading";
+import darkTheme from "../config/themes/dark";
 
 const Feed = (props) => {
   const history = useHistory();
-  // const { user } = props;
+  const { user } = props;
   const [feed, setFeed] = useState([]);
-  // const [playlist, setPlaylist] = useState([]);
-  // const [playlist, setPlaylist] = useState(play);
-  // const [currentUser, setCurrentUser] = useState(user);
+  const [loading, setLoading] = useState(true);
   
   useEffect(() => {
     async function fetchFeedData() {
       try {
-        
         const params = {
           pageNumber: 1,
           rowCount: 30,
-          userId: props.user.userId
-        }
-        const { data } = await dataService.getData('feed', params);
-        console.log('feed', data.body);
-        setFeed(data.body); 
-         
-      
-      }
-      
-      catch (e) {
+          userId: user.userId,
+        };
+        const { data } = await dataService.getData("feed", params);
+        console.log("feed", data.body);
+        setFeed(data.body);
+        setLoading(false);
+      } catch (e) {
         console.log(e);
         // toast.error('Something went wrong');
       }
     }
     fetchFeedData();
+  }, [user]);
 
-  }, [props.user]);
-
-  const handleAlbumClick = async data => {
+  const handleAlbumClick = async (data) => {
     // alert('Clicked');
     return history.push(`/content/${encode(data.contentId)}`);
-  }
+  };
 
   return (
-   <Container maxWidth='100%'>
-    <Grid container spacing={4}>
-        {feed &&
-          feed.map(f => (
-        <Grid item xs={12} lg={3} md={3}>
-          <CardComponent
-            data={f}
-            primaryData={f.title}
-            secondaryData={`by ${f.username}`}
-            onClick={handleAlbumClick}
-            image={storageURL + f.thumbnailLink}
-          />
-          <br />
-        
+    <Container maxWidth="100%">
+      {
+        loading ?
+        <div class="d-flex justify-content-center align-items-center">
+          <ReactLoading
+        type="bars"
+        color={darkTheme.primary}
+        height={200}
+            width={200}
           
-          </Grid>
-      ))}
-      </Grid>
-      </Container>
-  
-  )
-}
+            />
+            </div>
+          :
+          <Grid container spacing={4}>
+          {feed &&
+            feed.map((f) => (
+              <Grid item xs={12} lg={3} md={3}>
+                <CardComponent
+                  loading={loading}
+                  data={f}
+                  primaryData={f.title}
+                  secondaryData={`by ${f.username}`}
+                  onClick={handleAlbumClick}
+                  image={storageURL + f.thumbnailLink}
+                />
+                <br />
+              </Grid>
+            ))}
+        </Grid>
+      }
     
-    export default Feed
+    </Container>
+  );
+};
+
+export default Feed;
