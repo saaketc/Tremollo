8613913f12eac9d9f3744b7d3e79574a29e96e0d
Saaket <saaket123@outlook.com>
2020-07-02 11:17:20 +0530

    full reload in place of push

--- src/components/common/userTemplate.js
+++ src/components/common/userTemplate.js
@@ -2,20 +2,23 @@ import React from "react";
 import { Button, Avatar, Typography } from "@material-ui/core";
 import { storageURL } from "../../config/storage";
 import darkTheme from "../../config/themes/dark";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { encode } from "../../utils/utilfunctions";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   btn: {
     textTransform: "none",
+   
   },
+
 }));
 const UserTemplate = ({ user }) => {
-  const history = useHistory();
+  // const history = useHistory();
   const classes = useStyles();
+
   const handleClick = (user) => {
-    return history.push(`/profile/${encode(user.userId)}`);
+    return window.location = `/profile/${encode(user.userId)}`;
   };
 
   return (
