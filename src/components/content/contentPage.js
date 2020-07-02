4cd94007fd37bff0e06f3c9106f3008fbba6030c
Saaket <saaket123@outlook.com>
2020-07-02 12:04:28 +0530

    ket added

--- src/components/content/contentPage.js
+++ src/components/content/contentPage.js
@@ -47,7 +47,7 @@ const ContentPage = (props) => {
           contentId: contentId,
           userId: currentUser.userId,
         });
-        // console.log("content data", data.body);
+        console.log("content data", data.body);
         setContent(data.body);
         setLoading(false);
       } catch (e) {
