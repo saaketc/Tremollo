3506eca99210bd4206d45c6cc13e69837088dca0
Saaket <saaket123@outlook.com>
2020-07-02 11:50:55 +0530

    skeleton background color added

--- src/components/content/contentPage.js
+++ src/components/content/contentPage.js
@@ -18,10 +18,14 @@ import CenteredTabs from "../common/centredTabs";
 import colors from "../../config/colors";
 import { encode, decode } from "../../utils/utilfunctions";
 import { makeStyles } from "@material-ui/core/styles";
+import { darkTheme } from "../../config/Themes";
 
 const useStyles = makeStyles((theme) => ({
   btn: {
     textTransform: "none",
+  },
+  skeleton: {
+    backgroundColor: darkTheme.backgroundCard,
   },
 }));
 
@@ -41,7 +45,7 @@ const ContentPage = (props) => {
       try {
         const { data } = await dataServices.getData("content/withUser", {
           contentId: contentId,
-          userId: currentUser.userId
+          userId: currentUser.userId,
         });
         // console.log("content data", data.body);
         setContent(data.body);
@@ -62,113 +66,118 @@ const ContentPage = (props) => {
 
   return (
     <Container>
-   
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={9} lg={9}>
-            {loading ? (
-              <Skeleton animation="wave" variant="rect" height={600} />
-            ) : (
-              <VideoPlayer
-                url={storageURL + content.mediaLink}
-                thumbnailLink={storageURL + content.thumbnailLink}
-                width="800"
-                height="500"
-              />
-            )}
-            <BottomNav
-              data={{
-                followerId: currentUser.userId,
-                followedId: content.userId,
-                isFollowedByUser: content.isFollowedByUser,
-                isLikedByUser: content.isLikedByUser,
-                contentId: contentId,
-                likes: content.likes,
-              }}
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={9} lg={9}>
+          {loading ? (
+            <Skeleton
+              animation="wave"
+              variant="rect"
+              className={classes.skeleton}
+              height={600}
             />
-            <br />
-            <Grid container spacing={6}>
-              <Grid item xs={12} md={3} lg={3}>
-                <div style={{ display: "block" }}>
-                <Button
-                  className={classes.btn}
-                  onClick={handleUserClick}>
-                    {loading ? (
+          ) : (
+            <VideoPlayer
+              url={storageURL + content.mediaLink}
+              thumbnailLink={storageURL + content.thumbnailLink}
+              width="800"
+              height="500"
+            />
+          )}
+          <BottomNav
+            data={{
+              followerId: currentUser.userId,
+              followedId: content.userId,
+              isFollowedByUser: content.isFollowedByUser,
+              isLikedByUser: content.isLikedByUser,
+              contentId: contentId,
+              likes: content.likes,
+            }}
+          />
+          <br />
+          <Grid container spacing={6}>
+            <Grid item xs={12} md={3} lg={3}>
+              <div style={{ display: "block" }}>
+                <Button className={classes.btn} onClick={handleUserClick}>
+                  {loading ? (
+                    <Skeleton
+                      animation="wave"
+                      variant="circle"
+                      className={classes.skeleton}
+                      width={100}
+                      height={100}
+                    />
+                  ) : (
+                    <img
+                      style={smallPicStyle}
+                      src={storageURL + content.pageAvatar}
+                      alt="profile"
+                    />
+                  )}
+                  {loading ? (
+                    <>
                       <Skeleton
                         animation="wave"
-                        variant="circle"
+                        height={50}
                         width={100}
-                        height={100}
+                        className={classes.skeleton}
+                        style={{ marginBottom: 6, marginLeft: 6 }}
                       />
-                    ) : (
-                      <img
-                        style={smallPicStyle}
-                        src={storageURL + content.pageAvatar}
-                        alt="profile"
-                      />
-                    )}
-                    {loading ? (
-                      <>
-                        <Skeleton
-                          animation="wave"
-                          height={50}
-                          style={{ marginBottom: 6 }}
-                        />
-                        <Skeleton animation="wave" height={50} width="80%" />
-                      </>
-                    ) : (
-                      <Typography variant="h6" style={styles.title}>
-                        {content.username}
-                      </Typography>
-                    )}
+                    </>
+                  ) : (
+                    <Typography variant="h6" style={styles.title}>
+                      {content.username}
+                    </Typography>
+                  )}
                 </Button>
-                <p style={{color: colors.secondary, marginLeft:'10px'}}>{`${content.followers} fans`}</p>
-                
-                </div>
+                <p
+                  style={{ color: colors.secondary, marginLeft: "10px" }}
+                >{ loading ? 'Loading fans...' : `${content.followers} fans`}</p>
+              </div>
 
-                {/* Follow component */}
-              
-              </Grid>
-           
-              <Grid item xs={12} md={3} lg={3}>
+              {/* Follow component */}
+            </Grid>
+
+            <Grid item xs={12} md={3} lg={3}>
               <Follow
-                  followerId={currentUser.userId}
-                  followedId={content.userId}
-                  isFollowedByUser={content.isFollowedByUser}
-                />
-                <br/>
-                <br/>
-                </Grid>
+                followerId={currentUser.userId}
+                followedId={content.userId}
+                isFollowedByUser={content.isFollowedByUser}
+              />
+              <br />
+              <br />
+            </Grid>
           </Grid>
-                <Typography variant="h4" style={styles.title}>
-                  {content.title}
-                </Typography>
-                
-                <Typography variant="h6" style={styles.title}>
-                  {content.caption}
-                </Typography>
-            <br />
-            <br />
-            <br />
-            {/* Compliments component */}
-            <CenteredTabs
-              labels={["Compliments", "Liked by"]}
-              afterTabSet={afterTabSet}
-              maxWidth={830}
-            />
-            <br />
-            <br />
-            {tab === 0 && <Compliments contentId={contentId} currentUser={currentUser}/>}
-            {tab === 1 && <ContentLikes contentId={contentId} />}
+          <Typography variant="h4" style={styles.title}>
+            {content.title}
+          </Typography>
 
-            {/* <Grid item xs={12} md={6} lg={6}>
+          <Typography variant="h6" style={styles.title}>
+            {content.caption}
+          </Typography>
+          <br />
+          <br />
+          <br />
+          {/* Compliments component */}
+          <CenteredTabs
+            labels={["Compliments", "Liked by"]}
+            afterTabSet={afterTabSet}
+            maxWidth={830}
+          />
+          <br />
+          <br />
+          {tab === 0 && (
+            <Compliments contentId={contentId} currentUser={currentUser} />
+          )}
+          {tab === 1 && <ContentLikes contentId={contentId} />}
+
+          {/* <Grid item xs={12} md={6} lg={6}>
           <ContentLikes />
         </Grid> */}
-          </Grid>
-          <Grid item xs={12} md={3} lg={3}>
-            <QueuePlaylist removeContentId={contentId} user={currentUser} />
-          </Grid>
         </Grid>
-      
+        <Grid item xs={12} md={3} lg={3}>
+          <QueuePlaylist removeContentId={contentId} user={currentUser} />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
