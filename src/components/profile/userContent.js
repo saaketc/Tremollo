66632efd684b2edafb0607de304a8f21a9f0121a
Saaket <saaket123@outlook.com>
2020-07-02 12:51:25 +0530

    minor change

--- src/components/profile/userContent.js
+++ src/components/profile/userContent.js
@@ -1,42 +1,33 @@
-import React from 'react'
-import { Grid, Typography, Container } from '@material-ui/core';
+import React from "react";
+import { Grid, Typography, Container } from "@material-ui/core";
 
-import CardComponent from '../common/cardComponent';
-import { storageURL } from '../../config/storage';
+import CardComponent from "../common/cardComponent";
+import { storageURL } from "../../config/storage";
 
-const UserContent = ({content, onClick}) => {
-   return (
-       <Container maxWidth='xl'>
-            
-           {(content && content.length === 0) &&
-              <Typography variant='h5'>
-                   Ooops! No uploads yet.
-              </Typography>
-               
-          }
-               
-          <Grid container spacing={6}>
-         
-             {content &&
-            content.map((c) => (
-                
-                <Grid item xs={12} md={4} lg={4} key={c.title}>
-               
-                    <CardComponent
+const UserContent = ({ content, onClick }) => {
+  return (
+    <Container maxWidth="xl">
+      {content && content.length === 0 && (
+        <Typography variant="h5">Ooops! No uploads yet.</Typography>
+      )}
+
+      <Grid container spacing={6}>
+        {content &&
+          content.map((c) => (
+            <Grid item xs={12} md={4} lg={4} key={c.title}>
+              <CardComponent
                 data={c}
                 primaryData={c.title}
                 secondaryData={`${c.likesCount} likes`}
                 onClick={onClick}
-                  image={storageURL + c.thumbnailLink}
-                  width={300}
+                image={storageURL + c.thumbnailLink}
+                width={300}
               />
-                </Grid>
-    
-              ))}
-          </Grid>
-            
-            </Container>
-    )
-}
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
+  );
+};
 
-export default UserContent
+export default UserContent;
