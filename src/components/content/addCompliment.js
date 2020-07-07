a7e38f60fe957b8df3f5fadf48287706a12a2144
Saaket <saaket123@outlook.com>
2020-07-08 02:31:20 +0530

    alignment adjusted

--- src/components/content/addCompliment.js
+++ src/components/content/addCompliment.js
@@ -3,9 +3,9 @@ import TextForm from "../common/textForm";
 import dataServices from "../../services/dataServices";
 import { toast } from "react-toastify";
 import { Button, Container, Grid } from "@material-ui/core";
-import { buttonStyleOpen } from "../../config/buttonStyle";
+import { buttonStyleOpen, buttonStyleClose } from "../../config/buttonStyle";
 
-const AddCompliment = ({ complimentingUser, contentId, postSubmit  }) => {
+const AddCompliment = ({ complimentingUser, contentId, postSubmit }) => {
   const [compliment, setCompliment] = React.useState("");
 
   const handleChange = ({ currentTarget }) => {
@@ -15,8 +15,7 @@ const AddCompliment = ({ complimentingUser, contentId, postSubmit  }) => {
     e.preventDefault();
     try {
       let text = compliment.trim();
-      if (text === '')
-        return;
+      if (text === "") return;
       const toSubmit = {
         contentId,
         text,
@@ -27,31 +26,39 @@ const AddCompliment = ({ complimentingUser, contentId, postSubmit  }) => {
         toSubmit
       );
       setCompliment("");
-        postSubmit(data.body);
+      postSubmit(data.body);
     } catch (e) {
       toast.error("Something went wrong!");
     }
   };
   return (
     <Container>
-      <form onSubmit={handleSubmit} >
-       <Grid container spacing={2}>
-          <Grid item xs={12} md={6} lg={6}>
-          <TextForm
-          label={`Add a compliment as ${complimentingUser.username}`}
-          type="text"
-          name="compliment"
-          fullWidth
-          placeholder={`Write a compliment as ${complimentingUser.username}`}
-          value={compliment}
-          onChange={handleChange}
-              />
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} lg={12}>
+            <TextForm
+              label={`Add a compliment as ${complimentingUser.username}`}
+              type="text"
+              name="compliment"
+              // multiline
+              fullWidth
+              // placeholder={`Write a compliment as ${complimentingUser.username}`}
+              value={compliment}
+              onChange={handleChange}
+            />
           </Grid>
           <Grid item xs={12} md={4} lg={4}>
-          
-            <Button type='submit' style={buttonStyleOpen}>Add</Button>
-            </Grid>
+            <Button type="submit" style={buttonStyleOpen}>
+              Add
+            </Button>
+            <Button
+              style={{ marginLeft: "20px", ...buttonStyleClose }}
+              onClick={() => setCompliment("")}
+            >
+              Cancel
+            </Button>
           </Grid>
+        </Grid>
       </form>
     </Container>
   );
