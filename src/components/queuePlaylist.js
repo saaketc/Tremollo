7a59735a449b43675e59628fb24aaf387cfade87
Saaket <saaket123@outlook.com>
2020-07-14 18:03:40 +0530

    lazy loding added

--- src/components/queuePlaylist.js
+++ src/components/queuePlaylist.js
@@ -3,22 +3,26 @@ import Grid from "@material-ui/core/Grid";
 import dataService from "../services/dataServices";
 import { storageURL } from "../config/storage";
 import CardComponent from "./common/cardComponent";
-import { filter, encode, stringSlice } from "../utils/utilfunctions";
-import { Typography } from "@material-ui/core";
+import { encode, stringSlice } from "../utils/utilfunctions";
+import { Typography, Button } from "@material-ui/core";
 import ReactLoading from "react-loading";
 import colors from "../config/colors";
+import { buttonStyleOpen } from "../config/buttonStyle";
 
 const QueuePlaylist = (props) => {
   const { user, removeContentId } = props;
   const [queue, setQueue] = useState([]);
-  const [loading, setLoading] = React.useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [rowCount, setRowCount] = useState(12);
+  const [loading, setLoading] = useState(true);
+  const [loadmore, setLoadmore] = useState(false);
 
   useEffect(() => {
     async function fetchFeedData() {
       try {
         const params = {
           pageNumber: 1,
-          rowCount: 42,
+          rowCount: 12,
           userId: user.userId,
         };
         const { data } = await dataService.getData("feed", params);
@@ -37,6 +41,28 @@ const QueuePlaylist = (props) => {
     // alert('Clicked');
     return (window.location.href = `/content/${encode(data.contentId)}`);
   };
+  const loadMore = async () => {
+    try {
+      setLoadmore(true);
+      setPageNumber(pageNumber + 1);
+      const params = {
+        pageNumber: pageNumber + 1,
+        rowCount: rowCount,
+        userId: user.userId,
+      };
+      const { data } = await dataService.getData("feed", params);
+      // console.log('queue', data.body);
+      if (data.body.length === 0) return setLoadmore(false);
+      setQueue(() => {
+        return [...queue, ...data.body];
+      });
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      // toast.error('Something went wrong');
+    }
+  };
+
   return (
     <>
       <Typography variant="h4" style={{ fontWeight: 600, marginLeft: "10px" }}>
@@ -51,24 +77,34 @@ const QueuePlaylist = (props) => {
           width={100}
         />
       ) : (
-        filter(queue, "contentId", removeContentId).map((f) => (
-          <Grid container spacing={6}>
-            <Grid item xs={12} lg={3} md={3} key={f.title}>
-              <CardComponent
-                data={f}
-                primaryData={stringSlice(f.title)}
-                secondaryData={`by ${f.username}`}
-                tooltip={f.title}
-                tag={`${f.likes} likes`}
-                onClick={handleAlbumClick}
-                image={storageURL + f.thumbnailLink}
-                hover={false}
-                width={250}
-              />
-              <br />
+        <>
+          {queue.map((f) => (
+            <Grid container spacing={6}>
+              <Grid item xs={12} lg={3} md={3} key={f.title}>
+                <CardComponent
+                  data={f}
+                  primaryData={stringSlice(f.title)}
+                  secondaryData={`by ${f.username}`}
+                  tooltip={f.title}
+                  tag={`${f.likes} likes`}
+                  onClick={handleAlbumClick}
+                  image={storageURL + f.thumbnailLink}
+                  hover={false}
+                  width={250}
+                />
+                <br />
+              </Grid>
             </Grid>
-          </Grid>
-        ))
+          ))}
+          <br />
+          {!loadmore && (
+            <div className="d-flex justify-content-center align-items-center">
+              <Button onClick={loadMore} style={buttonStyleOpen}>
+                Load more
+              </Button>
+            </div>
+          )}
+        </>
       )}
     </>
   );
